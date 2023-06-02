a0b04e1714b5652b874e9189343fcd5616e14604
overphoenix <hpromatem@protonmail.com>
2023-06-02 18:47:49 +0300

    del `lib` dir

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as Amount } from "./bcoin/btc/amount";
-export { default as Address } from "./bcoin/primitives/address";
-export { default as Output } from "./bcoin/primitives/output";
-export { default as NodeClient } from "./node";
-export { default as WalletClient } from "./wallet";
